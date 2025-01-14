3f4d2a31449d9970ed92a9cc236ba007a23432e1
Ondra Schejbal <ondra.schejbal@gmail.com>
2025-01-14 23:18:44 +0100

    Oprava Homework / Lecture11

---
+++ radka_hw4/Homework_Lecture11/Homework/Produkt.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Homework_Lecture11
+{
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+
+
+        public Product(string name, decimal price, int quantity)
+        {
+            if (price < 0)
+                throw new InvalidProductException("Price cannot be negative.");
+           
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+
+
+}
---
+++ radka_hw4/Homework_Lecture11/InvalidProductException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Lecture11
+{
+
+
+    public class InvalidProductException : Exception
+    {
+        public InvalidProductException(string message) : base(message) { }
+    }
+
+}
